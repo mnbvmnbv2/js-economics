0005f937176aa32291335744315f5169e3f05612
mnbvmnbv2 <ferdinand@ytteborg.com>
2021-10-22 00:29:25 +0200

    working line chart

--- js.js
+++ js.js
@@ -1,32 +1,28 @@
-var arrData = [
-	{ id: 1, val: 200 },
-	{ id: 1, val: 150 },
-	{ id: 1, val: 280 },
-];
-
-var margin = { top: 20, right: 20, bottom: 30, left: 50 },
+// set the dimensions and margins of the graph
+var margin = { top: 20, right: 20, bottom: 50, left: 70 },
 	width = 960 - margin.left - margin.right,
 	height = 500 - margin.top - margin.bottom;
 
-var x = d3.scaleLinear().range([0, width]);
+// parse the date / time
+var parseTime = d3.timeParse('%d-%b-%y');
 
+// set the ranges
+var x = d3.scaleTime().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
 
-var xAxis = d3.axisBottom(x).tickFormat(function (d) {
-	return d.x;
-});
-
-var yAxis = d3.axisLeft(y);
-
-var line = d3
+// define the line
+var valueline = d3
 	.line()
 	.x(function (d) {
-		return x(d.id);
+		return x(d.date);
 	})
 	.y(function (d) {
-		return y(d.val);
+		return y(d.close);
 	});
 
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
 var svg = d3
 	.select('body')
 	.append('svg')
@@ -35,39 +31,35 @@ var svg = d3
 	.append('g')
 	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-var data = arrData.map(function (d) {
-	return {
-		id: d[0],
-		val: d[1],
-	};
+// Get the data
+d3.csv('data.csv').then(function (data) {
+	// format the data
+	data.forEach(function (d) {
+		d.date = parseTime(d.date);
+		d.close = +d.close;
+	});
+
+	// Scale the range of the data
+	x.domain(
+		d3.extent(data, function (d) {
+			return d.date;
+		})
+	);
+	y.domain([
+		0,
+		d3.max(data, function (d) {
+			return d.close;
+		}),
+	]);
+
+	// Add the valueline path.
+	svg.append('path').data([data]).attr('class', 'line').attr('d', valueline);
+
+	// Add the x Axis
+	svg.append('g')
+		.attr('transform', 'translate(0,' + height + ')')
+		.call(d3.axisBottom(x));
+
+	// Add the y Axis
+	svg.append('g').call(d3.axisLeft(y));
 });
-
-console.log(data);
-
-x.domain(
-	d3.extent(data, function (d) {
-		return d.id;
-	})
-);
-y.domain(
-	d3.extent(data, function (d) {
-		return d.val;
-	})
-);
-
-svg.append('g')
-	.attr('class', 'x axis')
-	.attr('transform', 'translate(0,' + height + ')')
-	.call(xAxis);
-
-svg.append('g')
-	.attr('class', 'y axis')
-	.call(yAxis)
-	.append('text')
-	.attr('transform', 'rotate(-90)')
-	.attr('y', 6)
-	.attr('dy', '.71em')
-	.style('text-anchor', 'end')
-	.text('Value');
-
-svg.append('path').datum(data).attr('class', 'line').attr('d', line);
